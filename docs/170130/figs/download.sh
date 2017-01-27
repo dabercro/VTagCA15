a7dff612fad5cbf1d635f8f19ce9d6259d721045
dabercro <daniel.r.abercrombie@gmail.com>
2017-01-27 17:44:16 -0500

    Started slides.

--- docs/170130/figs/download.sh
+++ docs/170130/figs/download.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-
+crombie downloadtar "http://t3serv001.mit.edu/~dabercro/plotviewer/returntar.php?only=pdf&files=170127%2Fcat_base_dRfj1Isob%2C170127%2Fcat_base_fj1MSD_corr"
+crombie downloadtar "http://t3serv001.mit.edu/~dabercro/plotviewer/returntar.php?only=pdf&files=170127%2Fcat_base%2BcloseB_fj1MSD_corr%2C170127%2Fcat_base%2BfarB_fj1MSD_corr"
+crombie downloadtar "http://t3serv001.mit.edu/~dabercro/plotviewer/returntar.php?only=pdf&files=170127%2Fcat_base%2BrightMass_dRfj1Isob"
+crombie downloadtar "http://t3serv001.mit.edu/~dabercro/plotviewer/returntar.php?only=pdf&files=170127%2Fcat_base%2BrightMass_fj1Tau21%2C170127%2Fcat_base%2BrightMass_fj1Tau21SD"
+crombie downloadtar "http://t3serv001.mit.edu/~dabercro/plotviewer/returntar.php?only=pdf&files=170127%2Fcat_base_N2DDT%2C170127_background%2Fcat_base_N2DDT%2C170127_down%2Fcat_base_N2DDT%2C170127_up%2Fcat_base_N2DDT%2C170127%2Fcat_base_fj1MSD_corr%2C170127_background%2Fcat_base_fj1MSD_corr%2C170127_down%2Fcat_base_fj1MSD_corr%2C170127_up%2Fcat_base_fj1MSD_corr"
+crombie downloadtar "http://t3serv001.mit.edu/~dabercro/plotviewer/returntar.php?only=pdf&files=170127%2Fcat_base_fj1M"
+crombie downloadtar "http://t3serv001.mit.edu/~dabercro/plotviewer/returntar.php?only=pdf&files=170127%2Fcat_fullcutz_fj1M%2C170127%2Fcat_fullcutz_fj1MSD_corr"
 
 for f in */*.pdf
 do
